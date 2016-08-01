92278af964ee60d25c2068f93ba0bc34d8c27833
Egor Khmelev <hmelyoff@gmail.com>
2016-08-01 16:16:10 +0300

    Refactor nprogress to match css naming pattern

--- src/js/utils/progress.js
+++ src/js/utils/progress.js
@@ -2,6 +2,18 @@ import nprogress from 'nprogress'
 import defer from 'lodash/defer'
 import '../../styles/nprogress.scss'
 
+nprogress.configure({
+  template: `
+    <div class="nprogress__bar" role="bar">
+      <div class="nprogress__peg"></div>
+    </div>
+    <div class="nprogress__spinner" role="spinner">
+      <div class="nprogress__spinner__icon"></div>
+    </div>
+  `
+})
+
+
 let index = 0
 
 function increment() {
