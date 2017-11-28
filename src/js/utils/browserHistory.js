604bdb7374e74060d8796da18391bb35b302780b
Egor Khmelev <hmelyoff@gmail.com>
2017-11-28 12:43:15 +0300

    Update browser history util

--- src/js/utils/browserHistory.js
+++ src/js/utils/browserHistory.js
@@ -1,7 +1,2 @@
-import { useRouterHistory } from 'react-router'
-import { createHistory } from 'history'
-
-const forceRefresh = /(iPhone|iPod|iPad).*AppleWebKit/i.test(window.navigator.userAgent)
-const history = useRouterHistory(createHistory)({ forceRefresh })
-
-export default history
+import createHistory from 'history/createBrowserHistory'
+export default createHistory()
