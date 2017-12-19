4f84866281af9779843e009b70a77d9e944184df
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2017-12-19 23:22:05 +0300

    Remove import classnames

--- components/Mount.js
+++ components/Mount.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
-import classnames from 'classnames'
 import get from 'lodash/get'
 import uniqueId from 'lodash/uniqueId'
 import pick from 'lodash/pick'
