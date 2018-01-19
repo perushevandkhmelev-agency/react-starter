002266c653dcdc36116723590373be736a39f1ff
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2018-01-19 03:13:44 +0300

    Arrow actions

--- actions/misc.js
+++ actions/misc.js
@@ -1,20 +1,16 @@
 import { SYNC_TIME, MISC_SET_LOCALE, MISC_SET_CONFIG } from 'constants/ActionTypes'
 
-export function syncTime() {
-  return {
-    type: SYNC_TIME,
-    data: Date.now()
-  }
-}
+export const syncTime = () => ({
+  type: SYNC_TIME,
+  data: Date.now()
+})
 
-export function setConfig(config) {
-  return {
-    type: MISC_SET_CONFIG,
-    data: config
-  }
-}
+export const setConfig = config => ({
+  type: MISC_SET_CONFIG,
+  data: config
+})
 
-export function setLocale(nextLocale, translations) {
+export const setLocale = (nextLocale, translations) => {
   return (dispatch, getState) => {
     const { locale } = getState()
     if (locale !== nextLocale) {
--- actions/router.js
+++ actions/router.js
@@ -1,23 +1,17 @@
 import { ROUTER_BEFORE_NAVIGATE, ROUTER_NAVIGATE, ROUTER_ERROR_RAISE } from 'constants/ActionTypes'
 
-export function beforeNavigate(state, initial) {
-  return {
-    type: ROUTER_BEFORE_NAVIGATE,
-    state: state,
-    initial: initial
-  }
-}
+export const beforeNavigate = (state, initial) => ({
+  type: ROUTER_BEFORE_NAVIGATE,
+  state: state,
+  initial: initial
+})
 
-export function navigate(state) {
-  return {
-    type: ROUTER_NAVIGATE,
-    state: state
-  }
-}
+export const navigate = state => ({
+  type: ROUTER_NAVIGATE,
+  state: state
+})
 
-export function raiseError(errorCode) {
-  return {
-    type: ROUTER_ERROR_RAISE,
-    errorCode: errorCode
-  }
-}
+export const raiseError = errorCode => ({
+  type: ROUTER_ERROR_RAISE,
+  errorCode: errorCode
+})
